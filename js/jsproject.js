caafb20e4ca50efccc6ce78d70aaf320052100f8
Maicon7500 <124109768+Maicon7500@users.noreply.github.com>
2023-04-30 20:53:20 -0300

    Update jsproject.js

--- js/jsproject.js
+++ js/jsproject.js
@@ -15,17 +15,3 @@ for (i = 0; i < acc.length; i++) {
 
 
 
-
-jQuery("document").ready(function($){
-    
-    var nav = $('.nav-container');
-    
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 136) {
-            nav.addClass("f-nav");
-        } else {
-            nav.removeClass("f-nav");
-        }
-    });
-
-});
